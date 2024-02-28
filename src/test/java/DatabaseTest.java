1346233781ae87da3f8fbafc1034c84f4a85009c
Victor Harbo Johnston <vholesen@hotmail.com>
2024-02-28 18:10:13 +0100

    Add test for getting a single book

--- src/test/java/DatabaseTest.java
+++ src/test/java/DatabaseTest.java
@@ -1,12 +1,25 @@
+import dk.harbojohnston.Book;
+import dk.harbojohnston.Database.Database;
 import dk.harbojohnston.Database.DatabaseActions;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 public class DatabaseTest {
 
     @Test
     public void testConnection() throws IOException {
         DatabaseActions.connectToDb();
+    }
+
+    @Test
+    public void testGetBook() throws Exception {
+        try (Database db = new Database()) {
+            Book result = db.getBookByAuthor("Doe");
+
+            System.out.println(result.toString());
+        }
+
     }
 }
